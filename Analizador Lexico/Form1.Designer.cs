5886cf1d6953e2c9a1b09c6dc507582cf42e61e9
HSamuelGomezC <gomezcasillash@gmail.com>
2024-09-03 17:36:45 -0600

    Se subio el proyecto

---
+++ Analizador Lexico/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Analizador_Lexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Analizar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Lexema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Numero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.entrada = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LR1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Analizar
+            // 
+            this.Analizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Analizar.Location = new System.Drawing.Point(727, 21);
+            this.Analizar.Name = "Analizar";
+            this.Analizar.Size = new System.Drawing.Size(75, 32);
+            this.Analizar.TabIndex = 1;
+            this.Analizar.Text = "Analizar";
+            this.Analizar.UseVisualStyleBackColor = true;
+            this.Analizar.Click += new System.EventHandler(this.Analizar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Lexema,
+            this.Token,
+            this.Numero});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(790, 301);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Lexema
+            // 
+            this.Lexema.HeaderText = "Lexema";
+            this.Lexema.Name = "Lexema";
+            this.Lexema.ReadOnly = true;
+            this.Lexema.Width = 250;
+            // 
+            // Token
+            // 
+            this.Token.HeaderText = "Token";
+            this.Token.Name = "Token";
+            this.Token.ReadOnly = true;
+            this.Token.Width = 250;
+            // 
+            // Numero
+            // 
+            this.Numero.HeaderText = "Numero";
+            this.Numero.Name = "Numero";
+            this.Numero.ReadOnly = true;
+            this.Numero.Width = 250;
+            // 
+            // entrada
+            // 
+            this.entrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.entrada.Location = new System.Drawing.Point(12, 24);
+            this.entrada.Name = "entrada";
+            this.entrada.Size = new System.Drawing.Size(701, 26);
+            this.entrada.TabIndex = 4;
+            this.entrada.TextChanged += new System.EventHandler(this.entrada_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(237, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Analizador Sintactico";
+            // 
+            // LR1
+            // 
+            this.LR1.Location = new System.Drawing.Point(413, 392);
+            this.LR1.Name = "LR1";
+            this.LR1.ReadOnly = true;
+            this.LR1.Size = new System.Drawing.Size(109, 20);
+            this.LR1.TabIndex = 6;
+            this.LR1.TextChanged += new System.EventHandler(this.LR1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 443);
+            this.Controls.Add(this.LR1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.entrada);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Analizar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Analizador Lexico y Sintactico";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Analizar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lexema;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Token;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Numero;
+        private System.Windows.Forms.TextBox entrada;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LR1;
+    }
+}
+
---
+++ Analizador Lexico/Form1.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analizador_Lexico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void entrada_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void Analizar_Click(object sender, EventArgs e)
+        {
+            string cadena0 = entrada.Text; // Obtener el texto del TextBox 'entrada'
+
+
+            List<Dictionary<string, object>> elementos = new List<Dictionary<string, object>>();
+            int estado = 0;
+            int indice = 0;
+            string cadena = cadena0 + '$';
+
+            while (indice <= (cadena.Length - 1) && estado == 0)
+            {
+                string lexema = "";
+                string token = "error";
+                int numero = -1;
+
+                while (indice <= (cadena.Length - 1) && estado != 20)
+                {
+                    if (estado == 0)
+                    {
+                        if (char.IsWhiteSpace(cadena[indice]))
+                        {
+                            estado = 0;
+                        }
+                        else if (char.IsDigit(cadena[indice]))
+                        {
+                            estado = 3;
+                            lexema += cadena[indice];
+                        }
+                        else if (char.IsLetter(cadena[indice]) || cadena[indice] == '_')
+                        {
+                            estado = 4;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '$')
+                        {
+                            estado = 20;
+                            lexema += cadena[indice];
+                            token = "pesos";
+                            numero = 18;
+                        }
+                        else if (cadena[indice] == '=')
+                        {
+                            lexema += cadena[indice];
+                            token = "asignación";
+                            numero = 8;
+                            estado = 5;
+                        }
+                        else if (cadena[indice] == '(')
+                        {
+                            token += "parentesisIz";
+                            numero = 4;
+                            estado = 20;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == ')')
+                        {
+                            token += "paretensisDer";
+                            numero = 5;
+                            estado = 20;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '{')
+                        {
+                            token += "llaveIz";
+                            numero = 6;
+                            estado = 20;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '}')
+                        {
+                            token += "llaveDer";
+                            numero = 7;
+                            estado = 20;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '>')
+                        {
+                            token += "opRelacional";
+                            numero = 17;
+                            estado = 6;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '<')
+                        {
+                            token += "opRelacional";
+                            numero = 17;
+                            estado = 7;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == ',')
+                        {
+                            token += "coma";
+                            numero = 3;
+                            estado = 20;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == ';')
+                        {
+                            token += "puntoComa";
+                            numero = 2;
+                            estado = 20;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '+' || cadena[indice] == '-')
+                        {
+                            token += "opSuma";
+                            numero = 14;
+                            estado = 20;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '*' || cadena[indice] == '/')
+                        {
+                            token += "opMul";
+                            numero = 16;
+                            estado = 20;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '!')
+                        {
+                            numero = 17;
+                            estado = 8;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '|')
+                        {
+                            numero = 17;
+                            estado = 9;
+                            lexema += cadena[indice];
+                        }
+                        else if (cadena[indice] == '&')
+                        {
+                            numero = 17;
+                            estado = 10;
+                            lexema += cadena[indice];
+                        }
+                        else
+                        {
+                            estado = 20;
+                            token = "error";
+                            lexema = cadena[indice].ToString();
+                        }
+                        indice++;
+                    }
+                    else if (estado == 3)
+                    {
+                        if (char.IsDigit(cadena[indice]))
+                        {
+                            estado = 3;
+                            token = "constante";
+                            numero = 13;
+                            indice++;
+                            lexema += cadena[indice];
+                        }
+                        else
+                        {
+                            estado = 20;
+                            // ...
+                        }
+                    }
+                    else if (estado == 4)
+                    {
+                        if (char.IsDigit(cadena[indice]) || char.IsLetter(cadena[indice]) || cadena[indice] == '_')
+                        {
+                            estado = 4;
+                            lexema += cadena[indice];
+                            token = "id";
+                            numero = 1;
+                            indice++;
+                        }
+                        else
+                        {
+                            estado = 20;
+                        }
+                    }
+                    else if (estado == 5)
+                    {
+                        if (cadena[indice] != '=')
+                        {
+                            estado = 20;
+                        }
+                        else
+                        {
+                            estado = 20;
+                            lexema += cadena[indice];
+                            token = "opRelacional";
+                            numero = 17;
+                            indice++;
+                        }
+                    }
+                    else if (estado == 6)
+                    {
+                        if (cadena[indice] != '=')
+                        {
+                            estado = 20;
+                        }
+                        else
+                        {
+                            estado = 20;
+                            lexema += cadena[indice];
+                            token += "opRelacional";
+                            indice++;
+                        }
+                    }
+                    else if (estado == 7)
+                    {
+                        if (cadena[indice] != '=')
+                        {
+                            estado = 20;
+                        }
+                        else
+                        {
+                            estado = 20;
+                            lexema += cadena[indice];
+                            token += "opRelacional";
+                            indice++;
+                        }
+                    }
+                    else if (estado == 8)
+                    {
+                        if (cadena[indice] != '=')
+                        {
+                            estado = 20;
+                        }
+                        else
+                        {
+                            estado = 20;
+                            lexema += cadena[indice];
+                            token += "opRelacional";
+                            indice++;
+                        }
+                    }
+                    else if (estado == 9)
+                    {
+                        if (cadena[indice] != '|')
+                        {
+                            estado = 20;
+                        }
+                        else
+                        {
+                            estado = 20;
+                            lexema += cadena[indice];
+                            token += "opLogica";
+                            indice++;
+                        }
+                    }
+                    else if (estado == 10)
+                    {
+                        if (cadena[indice] != '&')
+                        {
+                            estado = 20;
+                        }
+                        else
+                        {
+                            estado = 20;
+                            lexema += cadena[indice];
+                            token += "opLogica";
+                            indice++;
+                        }
+                    }
+                }
+                estado = 0;
+                elementos.Add(new Dictionary<string, object>
+            {
+                { "token", token },
+                { "lexema", lexema },
+                { "numero", numero }
+            });
+            }
+
+            foreach (var elemento in elementos)
+            {
+                if (elemento["lexema"].ToString() == "if")
+                {
+                    elemento["token"] = "condicional SI";
+                    elemento["numero"] = 9;
+                }
+                if (elemento["lexema"].ToString() == "while")
+                {
+                    elemento["token"] = "while";
+                    elemento["numero"] = 10;
+                }
+                if (elemento["lexema"].ToString() == "return")
+                {
+                    elemento["token"] = "return";
+                    elemento["numero"] = 11;
+                }
+                if (elemento["lexema"].ToString() == "else")
+                {
+                    elemento["token"] = "else";
+                    elemento["numero"] = 12;
+                }
+
+                //Console.WriteLine(elemento);
+            }
+
+            // Limpia el DataGridView antes de mostrar los resultados
+            dataGridView1.Rows.Clear();
+
+            // Agrega los resultados al DataGridView
+            foreach (var elemento in elementos)
+            {
+                string lexema = elemento["lexema"].ToString();
+                string token = elemento["token"].ToString();
+                int numero = (int)elemento["numero"];
+
+                dataGridView1.Rows.Add(lexema, token, numero);
+            }
+
+            LR1.Text = "NO Aceptada";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LR1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
